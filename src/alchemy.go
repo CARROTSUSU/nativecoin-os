a3a2354ac11d9e5f669804bcdbaf00c2337ef244
FOUNDER OF RCIT <animmmfarhanim@gmail.com>
2025-06-01 02:57:12 +0800

    Update alchemy.go

--- src/alchemy.go
+++ src/alchemy.go
@@ -1,1 +1,17 @@
+package alchemy
 
+import (
+    "context"
+    "fmt"
+    "github.com/ethereum/go-ethereum/ethclient"
+)
+
+func ConnectAlchemy(apiKey string) (*ethclient.Client, error) {
+    url := "https://eth-goerli.alchemyapi.io/v2/" + apiKey
+    client, err := ethclient.DialContext(context.Background(), url)
+    if err != nil {
+        return nil, err
+    }
+    fmt.Println("Connected to Alchemy Ethereum node")
+    return client, nil
+}
