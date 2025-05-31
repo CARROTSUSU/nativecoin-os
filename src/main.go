1e92d18ed2bd94c31388897223211cae2b55fdf8
FOUNDER OF RCIT <animmmfarhanim@gmail.com>
2025-06-01 03:01:57 +0800

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,42 +1,21 @@
-
 package main
 
 import (
     "fmt"
-    "log"
-    "nativecoin-os/alchemy"
-    "nativecoin-os/nativecoin"
     "os"
+    "nativecoin-os/p2p"
 )
 
 func main() {
-    apiKey := os.Getenv("ALCHEMY_API_KEY")
-    if apiKey == "" {
-        log.Fatal("Please set ALCHEMY_API_KEY env variable")
+    fmt.Println("Booting NativeCoin OS with P2P...")
+
+    h := p2p.StartNode()
+
+    if len(os.Args) > 1 {
+        // Connect to another peer via cmd argument
+        peerAddr := os.Args[1]
+        p2p.ConnectToPeer(h, peerAddr)
     }
 
-    client, err := alchemy.ConnectAlchemy(apiKey)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer client.Close()
-
-    fmt.Println("Starting NativeCoin OS kernel...")
-
-    coin := nativecoin.New()
-    coin.Mint("address1", 1000)
-    coin.Mint("address2", 500)
-
-    fmt.Printf("Balance address1: %d\n", coin.GetBalance("address1"))
-    fmt.Printf("Balance address2: %d\n", coin.GetBalance("address2"))
-
-    success := coin.Transfer("address1", "address2", 300)
-    if success {
-        fmt.Println("Transfer success")
-    } else {
-        fmt.Println("Transfer failed")
-    }
-
-    fmt.Printf("Balance address1: %d\n", coin.GetBalance("address1"))
-    fmt.Printf("Balance address2: %d\n", coin.GetBalance("address2"))
+    select {} // Keep node running
 }
