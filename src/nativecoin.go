be67e48dafbf1575b59af6b995ce80f14f85ffa9
FOUNDER OF RCIT <animmmfarhanim@gmail.com>
2025-06-01 02:57:34 +0800

    Update nativecoin.go

--- src/nativecoin.go
+++ src/nativecoin.go
@@ -1,1 +1,30 @@
+package nativecoin
+
+import "fmt"
+
+type NativeCoin struct {
+    balances map[string]uint64
+}
+
+func New() *NativeCoin {
+    return &NativeCoin{balances: make(map[string]uint64)}
+}
+
+func (nc *NativeCoin) GetBalance(addr string) uint64 {
+    return nc.balances[addr]
+}
+
+func (nc *NativeCoin) Mint(addr string, amount uint64) {
+    nc.balances[addr] += amount
+    fmt.Printf("Minted %d coins to %s\n", amount, addr)
+}
+
+func (nc *NativeCoin) Transfer(from, to string, amount uint64) bool {
+    if nc.balances[from] < amount {
+        return false
+    }
+    nc.balances[from] -= amount
+    nc.balances[to] += amount
+    return true
+}
 
